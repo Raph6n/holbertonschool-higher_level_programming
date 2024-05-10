835bc8cddcecbc86ecd7a841af7756cf5739831e
Raph6n <naim.raphael@outlook.fr>
2024-05-10 10:18:52 +0000

    correct task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 for i in range(10):
-    for j in range(i + 1, 10):
-        if i < j:
-            print("{:02d}{:02d}".format(i, j), end=", " if j < 9 or i < 8 else "\n")
+    for j in range(i, 10):
+        if i != j:
+            if i != 8 or j != 9:
+                print("{}{}, ".format(int(i), int(j)), end="")
+            else:
+                print("{}{}".format(int(i), int(j)), end="")
+print("")
