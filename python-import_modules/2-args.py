330df4808de4440b8d22a5c1c4f874b9031e041a
Raph6n <naim.raphael@outlook.fr>
2024-05-10 20:36:11 +0000

    tak 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,18 +1,15 @@
 #!/usr/bin/python3
 def print_arg(argv):
-    n = len(argv) - 1
+    n = len(argv) - 1  
     if n == 0:
         print("{:d} argument.".format(n))
-        return
+    elif n == 1:
+        print("{:d} argument:".format(n))
+        print("{:d}: {:s}".format(1, argv[1]))
     else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
+        print("{:d} arguments:".format(n))
+        for i in range(1, n + 1):
             print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
 
 if __name__ == "__main__":
     import sys
