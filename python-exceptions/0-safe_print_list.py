a9865cb6068abbec38a4cc010b20a194f2e2a03b
Raph6n <naim.raphael@outlook.fr>
2024-05-16 11:33:55 +0000

    correct task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x < 0 or x >= len(my_list):
-        print("Index is out of range.")
-    else:
-        print(my_list[x])
+    printed_elements = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end=" ")
+            printed_elements += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+    return printed_elements
