dcc81ae6ae4412a0f77fdb3b64205e2b8ab862e2
Raph6n <naim.raphael@outlook.fr>
2024-05-19 09:50:21 +0000

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""write empty class """
+"""Write an empty class """
+
 
 class Square:
+    """Define a square."""
     def __init__(self, size=0):
+        """Initialize a new Square.
 
-        """initialize new square """
+        Args:
+        size: size
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
