3ad3a825816506204c4065bc73d7e3ffb582fed9
Raph6n <naim.raphael@outlook.fr>
2024-05-20 09:32:43 +0000

    correct task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-def add_integer(a, b=98):
-    """ 
-    Add two integers or floats (casted to integers).
+"""
+This is the "Add Integer"  module.
 
-    Parameters:
-    a (int, float): The first number.
-    b (int, float): The second number, default is 98.
+This module supplies one function, add_integer(),
+which adds together 2 int or float types and returns an int.
+"""
 
-    Returns:
-    int: The sum of a and b after casting to integers.
 
-    Raises:
-    TypeError: If a or b are not integers or floats.
+def add_integer(a, b):
+    """Return the sum of two integers or floats as an integer.
+    Otherwise raise a TypeError for given incorrect argument type.
     """
-    # Check if `a` is either an integer or a float
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    
-    # Check if `b` is either an integer or a float
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
-    
-    # Cast both `a` and `b` to integers if they are floats
-    a = int(a)
-    b = int(b)
-    
-    # Return the sum of the two integers
-    return a + b
+    h = list(map(lambda x: isinstance(x, (int, float)), [a, b]))
+
+    if all(h):
+        return int(a) + int(b)
+
+    for x, y in list(zip(h, ['a', 'b'])):
+        if not x:
+            raise TypeError("{} must be an integer".format(y))
