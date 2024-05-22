e5cf1b9457bfca2b69837ee14bbb4de7ee0e6fa4
Raph6n <naim.raphael@outlook.fr>
2024-05-22 08:02:43 +0000

    printsquare without txt file

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_square(size):
+    """Prints a square with the character '#' of the given size."""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
