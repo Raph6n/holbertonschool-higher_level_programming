5c7db8c646043c72640e1367de2cbc32fea14d20
Raph6n <naim.raphael@outlook.fr>
2024-05-24 15:04:41 +0000

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents the base geometry class.
+    """
+
+    def area(self):
+        """
+        Calculates the area of the geometry.
+        """
+        raise Exception("area() is not implemented")
