c7cb4587cc4c9bc450e5f5040f660098243ce2ab
Raph6n <naim.raphael@outlook.fr>
2024-05-26 11:11:13 +0000

    task 0

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,1 +1,37 @@
-config
+#!/usr/bin/python3
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+    """
+    Abstract class representing an Animal.
+    """
+    @abstractmethod
+    def sound(self):
+        """
+        Abstract method that must be implemented by all subclasses.
+        This method should return the sound made by the animal.
+        """
+        pass
+
+class Dog(Animal):
+    """
+    Class representing a Dog.
+    Inherits from the Animal class and implements the sound method.
+    """
+    def sound(self):
+        """
+        Returns the sound made by a dog.
+        """
+        return "Bark"
+
+class Cat(Animal):
+    """
+    Class representing a Cat.
+    Inherits from the Animal class and implements the sound method.
+    """
+    def sound(self):
+        """
+        Returns the sound made by a cat.
+        """
+        return "Meow"
