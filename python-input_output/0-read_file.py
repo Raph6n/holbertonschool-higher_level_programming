c209050baf88ea6abf039f2478584d6e98f990c9
Raph6n <naim.raphael@outlook.fr>
2024-05-27 08:19:45 +0000

    add com task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+
 def read_file(filename=""):
-    with open(filename, encoding='utf-8') as f:
-        for line in f:
-            print(line, end='')
+    """
+    This function reads the content of a text file encoded in UTF-8
+    and prints the content to the standard output (stdout).
+
+    Parameters:
+    filename (str): the path to the file to read. If no path is provided, 
+                    an empty string will be used by default.
+    """
+    # Using the 'with' statement to manage the file
+    with open(filename, 'r', encoding='utf-8') as file:
+        # Read the file content and print it
+        print(file.read())
