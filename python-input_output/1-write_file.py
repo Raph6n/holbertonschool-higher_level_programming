af2a2ca4e924f80f961237c64120ffc538a72e95
Raph6n <naim.raphael@outlook.fr>
2024-05-27 11:25:19 +0000

    task 1 correct

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""A function that writes a string to a text file."""
+
+
 def write_file(filename="", text=""):
-    """Write a string to a text file in UTF-8 encoding and return the number of characters written."""
-    with open(filename, 'w', encoding='utf-8') as file:
-        num_chars_written = file.write(text)
-    return num_chars_written
+    """ Open a file and writes a string to a text file (UTF8) and returns the
+        number of characters written.
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        characters_written = f.write(text)
+    return characters_written
