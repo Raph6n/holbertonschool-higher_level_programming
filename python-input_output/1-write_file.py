59e5236c577f296c6339980211e903bb9a1a8a9a
Raph6n <naim.raphael@outlook.fr>
2024-05-27 11:22:09 +0000

    correct task 1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def write_file(filename="", text=""):
+    """Write a string to a text file in UTF-8 encoding and return the number of characters written."""
     with open(filename, 'w', encoding='utf-8') as file:
-        file.write(text)
-        return len(text)
+        num_chars_written = file.write(text)
+    return num_chars_written
