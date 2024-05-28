461de9cee23160a65b447b5613228dd76a53f036
Raph6n <naim.raphael@outlook.fr>
2024-05-28 07:22:34 +0000

    add coms t6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+"""Module for loading an object from a JSON file."""
 import json
 
-def load_from_json_file(filename):
-    """Creates an object from a JSON file."""
-    with open(filename, 'r') as file:
+def load_from_json_file(file_name):
+    """Load an object from a JSON file.
+    
+    Args:
+        file_name (str): The name of the JSON file.
+
+    Returns:
+        object: The object loaded from the JSON file.
+    """
+    # Open the file in read mode and specify UTF-8 encoding
+    with open(file_name, 'r', encoding='utf-8') as file:
+        # Load the object from the JSON file
         return json.load(file)
