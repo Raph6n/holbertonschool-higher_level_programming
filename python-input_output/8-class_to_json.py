b4d576170b22654c2fc7bb51d667c3a49051cb7a
Raph6n <naim.raphael@outlook.fr>
2024-05-28 13:01:16 +0000

    task 8

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
-""" A function that returns an object (Python data structure)
-    represented by a JSON string.
 """
+Returns the dictionary description with simple data structure
+for JSON serialization of an object.
+"""
+
 import json
 
-
 def class_to_json(obj):
-    """Returns a JSON representation of the given object."""
-    return json.dumps(obj.__dict__)
+    """
+    Converts an object's attributes to a dictionary that can be serialized to JSON.
+    
+    Parameters:
+    - obj: The object to convert.
+    
+    Returns:
+    - dict: A dictionary representation of the object's attributes suitable for JSON serialization.
+    """
+    desc = {}
+
+    for item_name in dir(obj):
+        if not item_name.startswith('__'):
+            item_value = getattr(obj, item_name)
+            if isinstance(item_value, (list, dict, str, int, bool)):
+                desc[item_name] = item_value
+
+    return desc
