376f53db12685fbb67a457797b6c98b09608c46e
Raph6n <naim.raphael@outlook.fr>
2024-05-29 16:57:03 +0000

    task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,36 @@
-cc
+#!/usr/bin/python3
+"""
+Module for basic serialization and deserialization of Python dictionaries
+"""
+
+import json
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Serializes a Python dictionary to a JSON file.
+
+    Args:
+        data (dict): The Python dictionary to serialize.
+        filename (str): The filename of the output JSON file.
+                        If the file already exists, it will be replaced.
+    """
+    # Open the file in write mode. If it exists, it will be overwritten.
+    with open(filename, 'w') as file:
+        # Serialize the dictionary to the file in JSON format.
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """
+    Loads and deserializes JSON data from a file to a Python dictionary.
+
+    Args:
+        filename (str): The filename of the input JSON file.
+
+    Returns:
+        dict: The deserialized Python dictionary.
+    """
+    # Open the file in read mode.
+    with open(filename, 'r') as file:
+        # Load and deserialize the JSON data from the file to a dictionary.
+        return json.load(file)
+
