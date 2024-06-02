8d8758d6454ac92af70cf31e225b408ee4ab2624
Raph6n <naim.raphael@outlook.fr>
2024-06-02 09:28:18 +0000

    correct task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,70 +1,51 @@
 #!/usr/bin/python3
+"""Develop a Simple API using Python with Flask"""
 from flask import Flask, jsonify, request
 
+
 app = Flask(__name__)
+users = {}
 
-# Dictionary to store users
-users = {
-    "jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}
-}
 
-# Route for the home page
-@app.route('/')
+@app.route("/")
 def home():
-    """
-    Home route.
-    Returns a welcome message.
-    """
-    return "Bienvenue sur l'API Flask !"
+    return "Welcome to the Flask API!"
 
-# Route to get the list of users
-@app.route('/data', methods=['GET'])
-def get_users():
-    """
-    Route to get the list of usernames.
-    Returns a JSON list of usernames.
-    """
-    return jsonify(list(users.keys()))
 
-# Route to check the API status
-@app.route('/status', methods=['GET'])
-def get_status():
-    """
-    Route to check the API status.
-    Returns "OK".
-    """
+@app.route("/data")
+def get_data():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+
+@app.route("/status")
+def status():
     return "OK"
 
-# Route to get user details
-@app.route('/users/<username>', methods=['GET'])
+
+@app.route("/users/<username>")
 def get_user(username):
-    """
-    Route to get user details.
-    Returns a JSON object with user details if the user exists, otherwise returns a 404 error.
-    """
-    if username in users:
-        return jsonify(users[username])
-    else:
-        return "User not found", 404
+    user = users.get(username)
+    if user:
+        return jsonify({"username": username, **user})
+    return jsonify({"error": "User not found"}), 404
 
-# Route to add a new user
-@app.route('/add_user', methods=['POST'])
+
+@app.route("/add_user", methods=["Post"])
 def add_user():
-    """
-    Route to add a new user.
-    Accepts a JSON object with user details in the request body.
-    Returns the added user details with a 201 status code if successful, or a 400 error if the user already exists.
-    """
     data = request.get_json()
-    username = data.get('username')
-    if username in users:
-        return "User already exists", 400
-    else:
-        users[username] = data
-        return jsonify(data), 201
 
-if __name__ == '__main__':
-    """
-    Main function to run the Flask app.
-    """
+    if not data or "username" not in data:
+        return jsonify({"error": "Username is required"}), 400
+
+    username = data["username"]
+    users[username] = {key: data[key] for key in ("name", "age", "city")}
+
+    return jsonify({
+        "message": "User added",
+        "user": {"username": username, **users[username]}
+        }), 201
+
+
+if __name__ == "__main__":
     app.run(debug=True)
